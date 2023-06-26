9985c91591db55d9bc2f7012a7e335769280b01b
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-06-26 16:14:24 +0100

    move readFile into controller and remove model for this endpoint

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -1,7 +1,8 @@
-const { selectApi } = require("../models/api.models");
+const fs = require("fs/promises");
 
 exports.getApi = (req, res, next) => {
-  selectApi()
+  fs.readFile(`${__dirname}/../endpoints.json`, "utf-8")
+    .then((fileData) => JSON.parse(fileData))
     .then((endpoints) => {
       res.status(200).send({ endpoints });
     })
--- models/api.models.js
+++
@@ -1,6 +0,0 @@
-const { log } = require("console");
-const fs = require("fs/promises");
-
-exports.selectApi = () => {
-  return fs.readFile(`${__dirname}/../endpoints.json`, "utf-8").then((fileData) => JSON.parse(fileData));
-};
