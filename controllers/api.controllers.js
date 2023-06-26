c0da5226d6d71e97446dcd75f4e6de2289a67ecc
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-06-26 15:26:39 +0100

    remove console.log

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -3,7 +3,6 @@ const { selectApi } = require("../models/api.models");
 exports.getApi = (req, res, next) => {
   selectApi()
     .then((endpoints) => {
-      console.log(endpoints);
       res.status(200).send({ endpoints });
     })
     .catch(next);
