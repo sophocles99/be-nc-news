919a5283a1fedde3350be675f792bb81a1a4ef19
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-06-27 18:21:36 +0100

    create GET /api/users route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {
   postCommentByArticleId,
   patchArticleById,
 } = require("./controllers/articles.controllers");
+const { getUsers } = require("./controllers/users.controllers");
 const {
   handleCustomErrors,
   handleServerErrors,
@@ -30,6 +31,12 @@ app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 
 app.patch("/api/articles/:article_id", patchArticleById);
+
+app.get("/api/users", getUsers);
+
+app.all("*", (_, res) => {
+  res.status(404).send({ msg: "Not found" });
+});
 
 app.use(handlePsqlErrors);
 
