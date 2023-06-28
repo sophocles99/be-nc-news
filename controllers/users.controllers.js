85c14c35fc1e9368f56a7cbc24854591580789ec
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-06-28 08:39:31 +0100

    create GET /api/users route

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -3,5 +3,5 @@ const { selectUsers } = require("../models/users.models");
 exports.getUsers = (req, res, next) => {
   selectUsers().then((users) => {
     res.status(200).send({ users });
-  });
+  }).catch(next);
 };
